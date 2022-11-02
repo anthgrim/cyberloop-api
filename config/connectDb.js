366d1c046edfb4675e126a2b9837efef80ea243d
Kevin Grimaldi <anthgrim@icloud.com>
2022-11-02 18:35:36 -0400

    function name mod

--- config/connectDb.js
+++ config/connectDb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 import * as dotenv from "dotenv";
 dotenv.config();
 
-const connectDb = async () => {
+const connectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URI, {
       useUnifiedTopology: true,
@@ -13,4 +13,4 @@ const connectDb = async () => {
   }
 };
 
-export default connectDb;
+export default connectDB;
